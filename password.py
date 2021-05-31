fe21128eadb4b86ed122a5c7df964d1addcec830
Tom Chan <tchan.cuhk@gmail.com>
2021-05-31 11:07:29 +0800

    update the file

--- password.py
+++ password.py
@@ -1,17 +1,14 @@
 # password for 3 trial
-password = input('Please input your password: ')
-i = 1
-if password == 'a123456':
-	print('Login successfully!')
-else:
-	while i < 3:
-		n = 3-i
+i = 0
+while i < 3:
+	password = input('Please input your password: ')
+	if password == 'a123456':
+		print('Login successfully!')
+		break
+	else: 
+		n = 2-i
 		print('Wrong password! you have', n, ' more chance')
-		password = input('Please input your password: ')
-		if password == 'a123456':
-			break
-		else:
-			i = i + 1
+		i = i + 1
 		
 
 	
